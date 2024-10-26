c92650be7025824d7a7548399a5d76e4eb2eb3f1
Nguyen Giang <67605679+nguyengiang2000@users.noreply.github.com>
2024-10-26 14:20:07 -0700

    Add Seperate Blog.js and classList event

--- my-profolio/index.js
+++ my-profolio/index.js
@@ -1,75 +1,16 @@
 "use strict";
 
-async function getRandomData(){
-    try {
-        const userNameElement = document.getElementById("RandomData");
-        const response = await fetch("https://randomuser.me/api/?results=1&nat=US");
+/*
 
-        // statusCheck
-        if (!response.ok) {
-            throw new Error(await response.text());        
-        }
+  Name: Nguyen Giang
+  Date: Oct 26, 2024
+  This is the index.html page for my portfolio of web development work.
+ */
 
-        const data = await response.json();
-        const users = data.results;
-
-        const user1 = `${users[0].name.first}`;
-        const user1_gender =  `${users[0].gender}`;
-        const  user1_place = `${users[0].location.state}`;
-      
-         // non trivial element
-        const newH4 = document.createElement("h4");
-
-         // non trivial element
-        newH4.textContent = user1 + ', (' + user1_gender + ") live in " + user1_place;
-        
-        // non trivial element
-        document.getElementById("follower").appendChild(newH4);
-
-    } catch (error) {
-        console.error(error);
-    }
+function setDarkMode(){
+    const element = document.getElementById("ThinkOutSide");
+    element.classList.toggle("addBorder");
 }
 
-function deleteButton() {
-    const followerDiv = document.getElementById("follower");
-    const h4Elements = followerDiv.querySelectorAll("h4"); 
-
-    h4Elements.forEach(h4 => {
-        h4.remove();
-    });
-
-}
-    
-// second async / promised
-function deleteContent() {
-    return new Promise((resolve) => {
-        alert("All characters will be removed. Please wait for awhile !");
-        setTimeout(() => {
-            deleteButton(); 
-            deleteNotify();
-            resolve();
-        }, 5000); 
-    });
-}
-
-// third async / promised
-function deleteNotify() {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            alert("All characters have been removed successfully!");
-            resolve();
-        }, 500); 
-    });
-}
-
-
-
-const fetchDataButton = document.getElementById("generateButton");
-const deleteDataButton = document.getElementById("deleteButton");
-
-
-fetchDataButton.addEventListener("click", getRandomData);
-
-
-deleteDataButton.addEventListener("click", deleteContent);
+const myButton = document.getElementById("myButton");
+myButton.onclick = setDarkMode;
