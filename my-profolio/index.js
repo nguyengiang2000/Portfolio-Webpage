f9eb528075756d0d3226fca9b03bb293b1e78be3
Nguyen Giang <67605679+nguyengiang2000@users.noreply.github.com>
2024-10-25 23:36:04 -0700

    Update Non Trivial

--- my-profolio/index.js
+++ my-profolio/index.js
@@ -1,12 +1,13 @@
-
+"use strict";
 
 async function getRandomData(){
     try {
         const userNameElement = document.getElementById("RandomData");
-            const response = await fetch("https://randomuser.me/api/?results=3&nat=US");
+        const response = await fetch("https://randomuser.me/api/?results=3&nat=US");
 
+        // statusCheck
         if (!response.ok) {
-            throw new Error("Failed to fetch data!");
+            throw new Error(await response.text());        
         }
 
         const data = await response.json();
@@ -23,13 +24,43 @@ async function getRandomData(){
         const user3 = `${users[2].name.first}`;
         const user3_gender =  `${users[2].gender}`;
         const  user3_place = `${users[2].location.state}`;
+         // non trivial element
+        const newH4 = document.createElement("h4");
+
+         // non trivial element
+        newH4.textContent = user1 + ', ' + user1_gender + " live in " + user1_place + 
+            " ----- " + user2 + ', ' + user2_gender + " live in " + user2_place + " ----- " +
+            user3 + ', ' + user3_gender + " live in " + user3_place;
         
-        userNameElement.textContent = user1 + ', ' + user1_gender + " live in " + user1_place + 
-        " ----- " + user2 + ', ' + user2_gender + " live in " + user2_place + " ----- " +
-        user3 + ', ' + user3_gender + " live in " + user3_place;
+        // non trivial element
+        document.getElementById("follower").appendChild(newH4);
 
     } catch (error) {
         console.error(error);
     }
 }
+
+function deleteButton() {
+    const followerDiv = document.getElementById("follower");
+    const h4Elements = followerDiv.querySelectorAll("h4"); 
+
+    h4Elements.forEach(h4 => {
+        h4.remove();
+    });
+
+}
+    
+// second async / promised
+function deleteContent() {
+    return new Promise((resolve) => {
+        alert("All characters will be removed. Please wait for awhile !");
+        setTimeout(() => {
+            deleteButton();
+            resolve();
+        }, 5000); 
+    });
+}
+
+
+
 
