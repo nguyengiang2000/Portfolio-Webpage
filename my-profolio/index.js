56cd10504fac6c09d9ed85335c102f6fc5b7ef9a
Nguyen Giang <67605679+nguyengiang2000@users.noreply.github.com>
2024-10-26 09:31:32 -0700

    Fixed JS index.js wrongposition

--- my-profolio/index.js
+++ my-profolio/index.js
@@ -3,7 +3,7 @@
 async function getRandomData(){
     try {
         const userNameElement = document.getElementById("RandomData");
-        const response = await fetch("https://randomuser.me/api/?results=3&nat=US");
+        const response = await fetch("https://randomuser.me/api/?results=1&nat=US");
 
         // statusCheck
         if (!response.ok) {
@@ -17,20 +17,11 @@ async function getRandomData(){
         const user1_gender =  `${users[0].gender}`;
         const  user1_place = `${users[0].location.state}`;
       
-        const user2 = `${users[1].name.first}`;
-        const user2_gender =  `${users[1].gender}`;
-        const  user2_place = `${users[1].location.state}`;
-
-        const user3 = `${users[2].name.first}`;
-        const user3_gender =  `${users[2].gender}`;
-        const  user3_place = `${users[2].location.state}`;
          // non trivial element
         const newH4 = document.createElement("h4");
 
          // non trivial element
-        newH4.textContent = user1 + ', ' + user1_gender + " live in " + user1_place + 
-            " ----- " + user2 + ', ' + user2_gender + " live in " + user2_place + " ----- " +
-            user3 + ', ' + user3_gender + " live in " + user3_place;
+        newH4.textContent = user1 + ', (' + user1_gender + ") live in " + user1_place;
         
         // non trivial element
         document.getElementById("follower").appendChild(newH4);
@@ -55,12 +46,30 @@ function deleteContent() {
     return new Promise((resolve) => {
         alert("All characters will be removed. Please wait for awhile !");
         setTimeout(() => {
-            deleteButton();
+            deleteButton(); 
+            deleteNotify();
             resolve();
         }, 5000); 
     });
 }
 
+// third async / promised
+function deleteNotify() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            alert("All characters have been removed successfully!");
+            resolve();
+        }, 500); 
+    });
+}
 
 
 
+const fetchDataButton = document.getElementById("generateButton");
+const deleteDataButton = document.getElementById("deleteButton");
+
+
+fetchDataButton.addEventListener("click", getRandomData);
+
+
+deleteDataButton.addEventListener("click", deleteContent);
