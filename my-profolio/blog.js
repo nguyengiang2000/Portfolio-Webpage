6a5fb926ca6612cab405946ecb2471c62b3e4bae
nguyen giang <giangthuynguyen2000@gmail.com>
2024-10-29 18:13:24 -0700

    add window load

--- my-profolio/blog.js
+++ my-profolio/blog.js
@@ -1,83 +1,77 @@
 "use strict";
 
 /*
-
   Name: Nguyen Giang
   Date: Oct 26, 2024
   This is the blog.js page for my portfolio of web development work.
  */
-async function getRandomData(){
-    try {
-        const userNameElement = document.getElementById("RandomData");
-        const response = await fetch("https://randomuser.me/api/?results=1&nat=US");
 
-        // statusCheck
-        if (!response.ok) {
-            throw new Error(await response.text());        
+window.addEventListener('load', function() {
+    async function getRandomData() {
+        try {
+            const userNameElement = document.getElementById("RandomData");
+            const response = await fetch("https://randomuser.me/api/?results=1&nat=US");
+
+            // statusCheck
+            if (!response.ok) {
+                throw new Error(await response.text());        
+            }
+
+            const data = await response.json();
+            const users = data.results;
+
+            const user1 = `${users[0].name.first}`;
+            const user1_gender = `${users[0].gender}`;
+            const user1_place = `${users[0].location.state}`;
+
+            // non-trivial element
+            const newH4 = document.createElement("h4");
+
+            // non-trivial element
+            newH4.textContent = user1 + ', (' + user1_gender + ") lives in " + user1_place;
+
+            // non-trivial element
+            document.getElementById("creativeBox").appendChild(newH4);
+
+        } catch (error) {
+            console.error(error);
         }
-
-        const data = await response.json();
-        const users = data.results;
-
-        const user1 = `${users[0].name.first}`;
-        const user1_gender =  `${users[0].gender}`;
-        const  user1_place = `${users[0].location.state}`;
-      
-         // non trivial element
-        const newH4 = document.createElement("h4");
-
-         // non trivial element
-        newH4.textContent = user1 + ', (' + user1_gender + ") live in " + user1_place;
-        
-        // non trivial element
-        document.getElementById("creativeBox").appendChild(newH4);
-
-    } catch (error) {
-        console.error(error);
     }
-}
 
-function deleteButton() {
-    const followerDiv = document.getElementById("creativeBox");
-    const h4Elements = followerDiv.querySelectorAll("h4"); 
+    function deleteButton() {
+        const followerDiv = document.getElementById("creativeBox");
+        const h4Elements = followerDiv.querySelectorAll("h4"); 
 
-    h4Elements.forEach(h4 => {
-        h4.remove();
-    });
+        h4Elements.forEach(h4 => {
+            h4.remove();
+        });
+    }
+        
+    // second async / promised
+    function deleteContent() {
+        return new Promise((resolve) => {
+            alert("All characters will be removed. Please wait for a while!");
+            setTimeout(() => {
+                deleteButton(); 
+                deleteNotify();
+                resolve();
+            }, 2000); 
+        });
+    }
 
-}
-    
-// second async / promised
-function deleteContent() {
-    return new Promise((resolve) => {
-        alert("All characters will be removed. Please wait for awhile !");
-        setTimeout(() => {
-            deleteButton(); 
-            deleteNotify();
-            resolve();
-        }, 2000); 
-    });
-}
+    // third async / promised
+    function deleteNotify() {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                alert("All characters have been removed successfully!");
+                resolve();
+            }, 500); 
+        });
+    }
 
-// third async / promised
-function deleteNotify() {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            alert("All characters have been removed successfully!");
-            resolve();
-        }, 500); 
-    });
-}
+    const fetchDataButton = document.getElementById("generateButton");
+    const deleteDataButton = document.getElementById("deleteButton");
 
-
-
-
-
-const fetchDataButton = document.getElementById("generateButton");
-const deleteDataButton = document.getElementById("deleteButton");
-
-fetchDataButton.onclick = getRandomData;
-deleteDataButton.onclick = deleteContent;
-
-
-
+    fetchDataButton.onclick = getRandomData;
+    deleteDataButton.onclick = deleteContent;
+});
--- my-profolio/index.js
+++ my-profolio/index.js
@@ -1,16 +1,17 @@
 "use strict";
 
 /*
-
   Name: Nguyen Giang
   Date: Oct 26, 2024
   This is the index.js page for my portfolio of web development work.
  */
 
-function setDarkMode(){
-    const element = document.getElementById("ThinkOutSide");
-    element.classList.toggle("addBorder");
-}
+window.addEventListener('load', function() {
+    function setBorderMode() {
+        const element = document.getElementById("ThinkOutSide");
+        element.classList.toggle("addBorder");
+    }
 
-const myButton = document.getElementById("myButton");
-myButton.onclick = setDarkMode;
+    const myButton = document.getElementById("myButton");
+    myButton.onclick = setBorderMode;
+});
