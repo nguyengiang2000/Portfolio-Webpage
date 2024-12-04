9bb93ba04bb3da4de5ad26ab943eb81d92e60ad1
nguyen giang <giangthuynguyen2000@gmail.com>
2024-12-04 14:54:11 -0800

    update Nav Bar

--- react-app/src/components/nav.jsx
+++ react-app/src/components/nav.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link, useNavigate } from 'react-router-dom'; 
 import './nav.css';
 
-function nav({ isLogin, setIsLogin, setCurrentUser, handleLogout }) {
+function nav({ isLogin, handleLogout }) {
   const navigate = useNavigate();
 
   const handleAddPost = () => {
